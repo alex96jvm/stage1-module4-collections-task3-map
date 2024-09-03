302c4d99ab0ff53456755d5c0f18038f2b374c61
alex96jvm <alex96jvm@gmail.com>
2024-09-03 17:54:31 +0300

    Complete task Map

--- src/main/java/com/epam/mjc/collections/map/FunctionValueFinder.java
+++ src/main/java/com/epam/mjc/collections/map/FunctionValueFinder.java
@@ -1,13 +1,25 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class FunctionValueFinder {
     public boolean isFunctionValuePresent(List<Integer> sourceList, int requiredValue) {
         Map<Integer, Integer> functionMap = calculateFunctionMap(sourceList);
+
+        for (Integer value : functionMap.values()) {
+            if (value == requiredValue) return true;
+        }
+        return false;
     }
 
     private Map<Integer, Integer> calculateFunctionMap(List<Integer> sourceList) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (Integer el : sourceList) {
+            map.put(el, 5 * el +2);
+        }
+        return map;
     }
 }
--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,18 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+        Map<String, Integer> map = new HashMap<>();
+        String[] array = sentence.replaceAll("\\pP", "").toLowerCase().split(" ");
+
+        for (String el : array){
+            if (!el.isBlank()) {
+                map.put(el, map.getOrDefault(el, 0) + 1);
+            }
+        }
+        return map;
     }
 }
